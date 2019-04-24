7b293d30c65d86193af87183e2bd3a6cd4143052
luud.janssen <luud.janssen@isaac.nl>
2019-04-24 14:33:48 +0200

    Use ecmaVersion and remove deprecated property

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ module.exports = {
     es6: true,
   },
   parserOptions: {
+    ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      experimentalObjectRestSpread: true,
       jsx: true,
     },
   },
